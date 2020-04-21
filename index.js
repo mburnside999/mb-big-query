b9533b5218b09d8d71f886baad771844322f3695
Mike Burnside <mburnside@mburnside-ltm2.internal.salesforce.com>
2020-04-22 08:43:55 +1000

    Test

--- index.js
+++ index.js
@@ -17,6 +17,12 @@ const options = {
     useLegacySql: false, // Use standard SQL syntax for queries.
   };
 
+const sqlQuery2="SELECT COUNT(*) c, JSON_EXTRACT_SCALAR(payload, '$.pull_request.base.repo.language') lang FROM `githubarchive.month.201801` WHERE JSON_EXTRACT_SCALAR(payload, '$.pull_request.base.repo.language')IS NOT NULL GROUP BY 2 ORDER BY 1 DESC LIMIT 10"
+const options2 = {
+  query: sqlQuery2,
+  timeoutMs: 10000, // Time out after 10 seconds.
+  useLegacySql: false, // Use standard SQL syntax for queries.
+};
 
 var app = express();
 
@@ -43,6 +49,23 @@ bigquery
     });
 
 })
+
+app.get('/bigquery2', function(request, response) {
+
+  var row;
+  bigquery
+      .query(options2)
+      .then(results => {
+        row=results[0][0];
+        console.log('Biq query returned: '+JSON.stringify(row));
+        response.send(JSON.stringify(row));
+      })
+      .catch(err => {
+        console.error('ERROR:', err);
+        response.send(err);
+      });
+  
+  })
 
 
 
